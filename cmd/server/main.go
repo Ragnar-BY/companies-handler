ba9664300bcc4576d94a0d06a97756187a883fe0
Roman <roman.buzuk@gmai.com>
2023-03-23 13:41:54 +0300

    added config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main (){
-	return
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ragnar-BY/companies-handler/internal/config"
+)
+
+func main() {
+	cfg, err := config.LoadConfig(".env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// Config is config struct
+type Config struct {
+	PostgreSQLAddr     string `env:"POSTGRESQL_ADDRESS"`
+	PostgreSQLUser     string `env:"POSTGRESQL_USER"`
+	PostgreSQLPassword string `env:"POSTGRESQL_PASSWORD"`
+	PostgreSQLDatabase string `env:"POSTGRESQL_DATABASE"`
+
+	ServerAddress string `env:"SERVER_ADDRESS"`
+}
+
+// LoadConfig loads config from .env file
+func LoadConfig(path string) (config Config, err error) {
+	err = cleanenv.ReadConfig(path, &config)
+	if errors.Is(err, os.ErrNotExist) {
+		err = cleanenv.ReadEnv(&config)
+	}
+	return
+}
