c877c3a522d018eaa46a9b6f6e99c78d96a790b1
Roman <roman.buzuk@gmai.com>
2023-03-24 18:25:24 +0300

    added ahutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/Ragnar-BY/companies-handler/internal/broker"
 	"github.com/Ragnar-BY/companies-handler/internal/config"
@@ -54,4 +56,17 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Fatal("Server forced to shutdown: ", zap.Error(err))
+	}
+	if err := dbClient.Close(); err != nil {
+		logger.Fatal("Database service forced to shutdown: ", zap.Error(err))
+	}
+
+	logger.Info("Server exiting")
 }
